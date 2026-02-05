fb385ee544d1668c093134e6adc2d2a421a54943
PatVolk <59589443+PatVolk@users.noreply.github.com>
2026-02-05 14:00:24 -0800

    Fixed Gyro initialization bug. Turns out using Timer.delay() is still bad (always bad!). Don't even be termpted to use it! Should have known better... Anyway, the fix was to eliminate waitForStartup(), buut still suppress boot up errors by tracking time in update() and not making any refresh() calls until the Pigeon2 is almost certain to be fully booted up.

--- src/main/java/frc/lib/Sensors/GyroIO.java
+++ src/main/java/frc/lib/Sensors/GyroIO.java
@@ -2,6 +2,7 @@ package frc.lib.Sensors;
 
 import com.ctre.phoenix6.StatusCode;
 import com.ctre.phoenix6.hardware.Pigeon2;
+import com.revrobotics.servohub.config.ServoHubConfigAccessor;
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.Pigeon2Configuration;
 import com.ctre.phoenix6.BaseStatusSignal;
@@ -14,21 +15,15 @@ import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-/*
- * WARNING! GyroIO is still under development. Not working right as
- * of Feb 4, 2026. As a separate class, for some reason, 
- * it does not play well on the CAN bus, resulting in stale frames.
- * Works in test programs, so not a hardware or wiring probllem.
- */
 public class GyroIO {
     private Pigeon2 m_gyro;
     private boolean m_isInverted;
-    double m_now = Timer.getFPGATimestamp();
-    private boolean m_isWorking = false;
+    private double m_startupTime = Timer.getFPGATimestamp();
+    private boolean m_isInitialized =  false;
+    private double INITIALIZATION_QUIET_TIME = 0.5;        // seconds
 
     // StatusSignals
     private final StatusSignal<Angle> m_yawSignal;
-/*
     private final StatusSignal<Angle> m_pitchSignal;
     private final StatusSignal<Angle> m_rollSignal;
     private final StatusSignal<LinearAcceleration> m_accelXSignal;
@@ -36,7 +31,7 @@ public class GyroIO {
     private final StatusSignal<AngularVelocity> m_angVelZDeviceSignal;
     private final StatusSignal<AngularVelocity> m_angVelZWorldSignal;
     private final StatusSignal<Temperature> m_gyroTempSignal;
-*/
+
     public GyroIO(int canID, boolean isInverted, CANBus canbusToUse) {
         m_gyro = new Pigeon2(canID, canbusToUse);
         m_isInverted = isInverted;                  // Not currently implemented, becuase
@@ -45,9 +40,9 @@ public class GyroIO {
                                                     // ignored for now. But will need to 
                                                     // be implemented if
                                                     // NavX is once again used.
-        // Create all signals immediately (safe even before Config)
+        // Create all signals immediately (safe even before Config - these do not
+        // involve any CANbus traffic)
         m_yawSignal  = m_gyro.getYaw();
-/*
         m_pitchSignal = m_gyro.getPitch();
         m_rollSignal  = m_gyro.getRoll();
         m_accelXSignal = m_gyro.getAccelerationX();
@@ -55,39 +50,10 @@ public class GyroIO {
         m_angVelZDeviceSignal = m_gyro.getAngularVelocityZDevice();
         m_angVelZWorldSignal  = m_gyro.getAngularVelocityZWorld();
         m_gyroTempSignal = m_gyro.getTemperature();
-*/
-        // Wait for the Pigeon2 to come online (suppress startup errors)
-        waitForStartup();
-        configPigeon2Gyro();
+
+        m_isInitialized = false;
     }
 
-    //
-    // Wait for the Gyro to finish booting, suppressing all transient error 
-    // messages to the terminal, but do annouce when it fails to launch in a 
-    // timely manner.
-    //
-    private void waitForStartup() {
-        StatusCode code;
-
-        while (!m_isWorking) {
-            m_yawSignal.refresh();
-            code = m_yawSignal.getStatus();
-            m_now = Timer.getFPGATimestamp();
-            SmartDashboard.putNumber("Yaw on startup", m_gyro.getYaw().getValueAsDouble());
-            SmartDashboard.putNumber("Startup Time", m_now);
-            if (code.isOK()) {
-                m_isWorking = true;
-            } else if (m_now < 30.0) {      // expexted boot time is a few seconds at most. Wait 30 sec during debug
-                System.out.println("Pigeon2 still attempting to start @: " +
-                                    Timer.      getFPGATimestamp()+"  "+code.getDescription()); 
-                Timer.delay(0.05);// Wait 50 ms
-            } else {
-                System.out.println("Pigeon2 failed to start: ");
-                break;
-            }
-        }
-    }
-    
     // The following configures the gyro, ensures it is zeroed, and optimizesBusUtilization.
 
     private void configPigeon2Gyro() {
@@ -108,71 +74,101 @@ public class GyroIO {
     //
     public void zeroGyro() {
         m_gyro.reset();
-        m_yawSignal.refresh();
     }
 
     //
     // Call gyro.update() once per robot loop - insert into Robot.java
     //
     public void update() {
-        StatusCode code;
-        if (m_isWorking) {
-           code = BaseStatusSignal.refreshAll( m_yawSignal 
-                                                    //, m_pitchSignal, m_rollSignal,
-                                                    // m_accelXSignal, m_accelYSignal,
-                                                    // m_angVelZDeviceSignal, m_angVelZWorldSignal,
-                                                    // m_gyroTempSignal
-                                                );
-            if (! code.isOK()) {
-                System.out.println("Pigeon2 runtime error: " + code.getDescription());
-            }
-            // Optional - publish gyro data here. If so, put in a timer to slow the 
-            // rate to a max of 10 hz. However, usually pertinent Gyro data will be 
-            // published by those subsystem(s) that need gyro sensor data to function...
+        double now = Timer.getFPGATimestamp();      // First, read the time
+
+        // During startup period: never refresh
+        if (now - m_startupTime < INITIALIZATION_QUIET_TIME) {
+            // For the first INITIALIZATION_QUIET_TIME seconds, suppress error messages from
+            // the Pigeon2 by ensuring no refresch calls are made during this time.
+            // Apparently only the refresh() or refreshAll() methods trigger error messages.
+            // This will help keep the startup console quieter, hopefully.
+            return; 
         }
-    }
+
+        // After the startup period has passed, just assume Pigeon2 hardware initialization is 
+        // sucessful and complete - either way, go ahead and start / continue refreshing, and 
+        // report all errors encountered, but if our internal m_isInitialized flag isstill false,
+        // there is a little bit of cogfiguring still to do,foloowed by clearing the flag.
+
+        // Now, since we're past the startup time, do a refreshAll()
+        StatusCode code = BaseStatusSignal.refreshAll( m_yawSignal, m_pitchSignal, m_rollSignal,
+                                                       m_accelXSignal, m_accelYSignal,
+                                                       m_angVelZDeviceSignal, m_angVelZWorldSignal,
+                                                       m_gyroTempSignal
+                                                     );
+
+        // And do normal runtime error reporting, if appropriate
+        if ( !code.isOK() ) {
+            System.out.println("Pigeon2 runtime error: " + code.getDescription());
+        } else {                        
+            // No error, erverthing is OK. If init is not finished, do it now and report time
+            if (! m_isInitialized) {
+                configPigeon2Gyro();
+                zeroGyro();
+                m_isInitialized = true;
+                // To fine tune how long it takes the Pigeon2 to boot, set INITIALIZE_QUIET_TIME 
+                // to a very low number, or even 0
+                System.out.println("Pigeon2 fully initialized at time: " + now);
+            } else {
+                // We're basically done, but...
+                // Optional - publish gyro data here. If so, use the time already sampled to slow the 
+                // print rate to a max of 10 hz. However, usually pertinent Gyro data will be 
+                // published by those subsystem(s) that need the gyro sensor data to function...
+                // So publishing here might be redundant
+            }
+        }
+    }   
 
     // ------------------ Getters ------------------
+    // If called before initialization is complete, they generall 
+    // return either 0 (probably what we want to initialize mechanisms
+    // with)), or the last data value cached.
 
     public double getYawDegrees() {
         return MathUtil.inputModulus(m_yawSignal.getValueAsDouble(), -180.0, 180.0);
     }
-/*
+
     public double getPitchDegrees() {
-        return 0.0;  //m_pitchSignal.getValueAsDouble();
+        return m_pitchSignal.getValueAsDouble();
     }
 
     public double getRollDegrees() {
-        return 0.0;  //m_rollSignal.getValueAsDouble();
+        return m_rollSignal.getValueAsDouble();
     }
 
     public double getAccelerationX() {
-        return 0.0;  //m_accelXSignal.getValueAsDouble();
+        return m_accelXSignal.getValueAsDouble();
     }
 
     public double getAccelerationY() {
-        return 0.0;  //m_accelYSignal.getValueAsDouble();
+        return m_accelYSignal.getValueAsDouble();
     }
 
     public double getAngularVelocityZ_Device() {
-        return 0.0;  //m_angVelZDeviceSignal.getValueAsDouble();
+        return m_angVelZDeviceSignal.getValueAsDouble();
     }
 
     public double getAngularVelocityZ_World() {
-        return 0.0;  //m_angVelZWorldSignal.getValueAsDouble();
+        return m_angVelZWorldSignal.getValueAsDouble();
     }
 
     public double getTemperatureC() {
-        return 0.0;  //m_gyroTempSignal.getValueAsDouble();
+        return m_gyroTempSignal.getValueAsDouble();
     }
-*/
+
     public Rotation2d getRotation2d() {
         return Rotation2d.fromDegrees(0.0);     //getYawDegrees());
-        // Might be possible to do a direct: return m_gyro.getRotation2d();
+        // Might be possible to do a direct Rotation2d fetch: return m_gyro.getRotation2d();
         // but it is not clear how efficient that would be wrt canbus
         // optimiztion and refreshing
     }
-/*
+
     public Rotation3d getRotation3d() {
         return new Rotation3d(
             Math.toRadians(0.0),         //getRollDegrees()),
@@ -180,6 +176,5 @@ public class GyroIO {
             Math.toRadians(0.0)         //getYawDegrees())
         );
     }
-*/
 }
 
